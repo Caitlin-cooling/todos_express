a850ebfc984fc801b12363fff4f0dc37f96a6fcf
Caitlin Cooling <ccooling97@gmail.com>
2019-08-10 14:14:08 +0100

    Adds first todo route

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const todoRoutes = require('./routes/todos')
 
 app.get('/', function (req, res) {
   res.send('Hi there from express')
 })
+
+// use the routes and define a prefix for all
+app.use('/api/todos', todoRoutes)
 
 app.listen(port, function () {
   console.log('Hello world')
