a5d30bb8c81fed0a12ef406ab0442f1c6fab00a1
Caitlin Cooling <ccooling97@gmail.com>
2019-08-10 14:34:19 +0100

    Adds body-parser to allow access to request body

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const todoRoutes = require('./routes/todos')
+const bodyParser = require('body-parser')
 
 app.get('/', function (req, res) {
   res.send('Hello from root route')
 })
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 // use the routes and define a prefix for all
 app.use('/api/todos', todoRoutes)
 
