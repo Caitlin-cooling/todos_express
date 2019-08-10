d21e898296d0c4e038409e55fde46e8f2c87e101
Caitlin Cooling <ccooling97@gmail.com>
2019-08-10 14:44:46 +0100

    Post route creates a new entry in the db'

--- routes/todos.js
+++ routes/todos.js
@@ -3,11 +3,19 @@ const router = express.Router()
 const db = require('../models')
 
 router.get('/', function(req, res) {
-  // note that Todo is because this is being exported from todo.js
-  // which is being called in index.js
   db.Todo.find()
     .then(function(res){
       res.json(todos)
+    })
+    .catch(function(error) {
+      res.send(error)
+    })
+})
+
+router.post('/', function(req, res) {
+  db.Todo.create(req.body)
+    .then(function(newTodo) {
+      res.status(201).json(newTodo)
     })
     .catch(function(error) {
       res.send(error)
