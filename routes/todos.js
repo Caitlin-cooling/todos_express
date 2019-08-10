f7050e9f2b19aec750265f3130ae39adfb8b3d18
Caitlin Cooling <ccooling97@gmail.com>
2019-08-10 14:52:50 +0100

    Get route to show single todo

--- routes/todos.js
+++ routes/todos.js
@@ -4,7 +4,7 @@ const db = require('../models')
 
 router.get('/', function(req, res) {
   db.Todo.find()
-    .then(function(res){
+    .then(function(todos){
       res.json(todos)
     })
     .catch(function(error) {
@@ -16,6 +16,16 @@ router.post('/', function(req, res) {
   db.Todo.create(req.body)
     .then(function(newTodo) {
       res.status(201).json(newTodo)
+    })
+    .catch(function(error) {
+      res.send(error)
+    })
+})
+
+router.get('/:todoId', function(req, res) {
+  db.Todo.findById(req.params.todoId)
+    .then(function(todo) {
+      res.json(todo)
     })
     .catch(function(error) {
       res.send(error)
