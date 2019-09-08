39b70bc18684c5cafa83578d4ddb6a05d9dca795
Caitlin Cooling <ccooling@deloitte.co.uk>
2019-09-08 11:23:03 +0100

    Root route now renders an html file

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const port = 3000
 const todoRoutes = require('./routes/todos')
 const bodyParser = require('body-parser')
 
-app.get('/', function (req, res) {
-  res.send('Hello from root route')
-})
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 // use the routes and define a prefix for all
+app.use(express.static(__dirname + '/views'))
 app.use('/api/todos', todoRoutes)
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html')
+})
 
 app.listen(port, function () {
   console.log('Hello world')
